702a5c6a09b3485634fd2b4a19b21f39b0fe9b44
Prilog <sementrapeznikov@gmail.com>
2020-05-26 14:30:06 +0500

    apply requested changes(except one)

--- ITMO_FS/filters/multivariate/SPEC.py
+++ ITMO_FS/filters/multivariate/SPEC.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sklearn as sl
 from scipy.sparse import csgraph
 
 
@@ -23,10 +24,10 @@ class SPEC(object):
     """
 
     def __init__(self):
-        self.__scores = None
+        self.__desc = None
         self.__ranked_features = None
 
-    def __score_features(self, X, measure, style):
+    def __score_features(self, X, measure, k):
         """
             Computes score of each feature.
 
@@ -38,10 +39,10 @@ class SPEC(object):
                 A metric name defined in GLOB_MEASURE or a callable with
                 signature measure(selected_features, free_features, dataset, labels)
                 which should return a list of metric values for each feature in the dataset.
-            style : integer number of scoring type
-                -1      - score = f'Lf. Measures the value of the normalized cut by using f as
+            k : integer number of scoring type
+                k = -1  - score = f'Lf. Measures the value of the normalized cut by using f as
                             the soft cluster indicator to partition the graph G.
-                0       - using all eigenvalues except the first. Small score indicates that feature aligns closely
+                k = 0   - using all eigenvalues except the first. Small score indicates that feature aligns closely
                             to those nontrivial eigenvalues, hence provides good separability.
                 k > 0   - using first k except the first. Achieves an effect of reducing noise.
 
@@ -50,30 +51,24 @@ class SPEC(object):
             None
         """
 
-        if style < -1:
-            return
+        if k < -1:
+            raise Exception('k must me >= -1')
 
         n_samples, n_features = X.shape
 
         # building adjacency matrix
-        W = np.zeros((n_samples, n_samples))
-        for i in range(n_samples):
-            for j in range(n_samples):
-                if i != j:
-                    W[i][j] = measure(X[i], X[j])
+        W = np.array([[measure(i, j) for i in X] for j in X])
+        W = W - np.diag(np.diag(W))
 
         # building degree matrix
-        d = np.array(W.sum(axis=1))
-        D = np.zeros((n_samples, n_samples))
-        for i in range(n_samples):
-            D[i, i] = d[i]
+        D = np.diag(W.sum(axis=1))
 
         # building normalized laplacian matrix and calculating spectral information
         L_norm = csgraph.laplacian(W, normed=True)
         s, U = np.linalg.eigh(L_norm)
 
         # selecting features
-        __scores = np.ones(n_features) * 1000
+        __desc = np.ones(n_features) * 1000
         D2 = np.power(D, 0.5)
 
         for i in range(n_features):
@@ -81,7 +76,7 @@ class SPEC(object):
             D1 = D2.dot(f)
             D1_norm = np.linalg.norm(D1)
             if D1_norm < 100 * np.spacing(1):
-                __scores[i] = 1000
+                __desc[i] = 1000
                 continue
             else:
                 D1 = D1 / D1_norm
@@ -90,44 +85,44 @@ class SPEC(object):
             a = np.multiply(a, a)
             a = np.transpose(a)
 
-            if style == -1:
-                __scores[i] = np.transpose(D1).dot(L_norm).dot(D1)
-            elif style == 0:
-                __scores[i] = (np.transpose(D1).dot(L_norm).dot(D1))/(np.transpose(D1).dot(U[0]))
+            if k == -1:
+                __desc[i] = np.transpose(D1).dot(L_norm).dot(D1)
+            elif k == 0:
+                __desc[i] = (np.transpose(D1).dot(L_norm).dot(D1))/(np.transpose(D1).dot(U[0]))
             else:
-                a1 = a[n_samples-style:n_samples-1]
-                __scores[i] = np.sum(a1 * (2 - s[n_samples-style: n_samples - 1]))
+                a1 = a[n_samples-k:n_samples-1]
+                __desc[i] = np.sum(a1 * (2 - s[n_samples-k: n_samples - 1]))
 
-        if style > 0:
-            __scores[__scores == 1000] = -1000
-        self.__scores = __scores
+        if k > 0:
+            __desc[__desc == 1000] = -1000
+        self.__desc = __desc
 
-    def __rank_features(self, score, style):
+    def __rank_features(self, score, k):
         """
-            Ranks features depending on style.
+            Ranks features depending on k.
 
             Parameters
             ----------
             score : numpy array, shape (n_features)
                 Scores of features, computed by SPEC.
-            style : integer number of scoring type. Defines how features should be ranked.
-                -1      - ranking in descending order, the higher the score, the more relevant feature is.
-                0       - ranking in descending order, the higher the score, the more relevant feature is.
+            k : integer number of scoring type. Defines how features should be ranked.
+                k = -1  - ranking in descending order, the higher the score, the more relevant feature is.
+                k = 0   - ranking in descending order, the higher the score, the more relevant feature is.
                 k > 0   - ranking in ascending order, the lower the score, the more relevant feature is.
 
             Returns
             ----------
             None
         """
-        if style < -1:
-            return
+        if k < -1:
+            raise Exception('k must me >= -1')
         sorted_score = np.argsort(score)
-        if style > 0:
+        if k > 0:
             self.__ranked_features = sorted_score
         else:
             self.__ranked_features = sorted_score[::-1]
 
-    def run(self, X, measure, style):
+    def run(self, X, measure, k):
         """
             Implements SPEC feature selection.
 
@@ -139,10 +134,10 @@ class SPEC(object):
                 A metric name defined in GLOB_MEASURE or a callable with
                 signature measure(selected_features, free_features, dataset, labels)
                 which should return a list of metric values for each feature in the dataset.
-            style : integer number of scoring type. Also influences ranking.
-                -1      - score = f'Lf. Measures the value of the normalized cut by using f as
+            k : integer number of scoring type. Also influences ranking.
+                k = -1  - score = f'Lf. Measures the value of the normalized cut by using f as
                             the soft cluster indicator to partition the graph G. Ranked in descending order.
-                0       - using all eigenvalues except the first. Small score indicates that feature aligns closely
+                k = 0   - using all eigenvalues except the first. Small score indicates that feature aligns closely
                             to those nontrivial eigenvalues, hence provides good separability.
                             Ranked in descending order.
                 k > 0   - using first k except the first. Achieves an effect of reducing noise.
@@ -150,11 +145,11 @@ class SPEC(object):
 
             Returns
             ----------
-            scores: numpy array, shape (n_features)
-                Scores of features, depending on style.
+            desc: numpy array, shape (n_features)
+                Scores of features, depending on k.
             ranked_features: numpy array, shape (n_features_
-                Sorted list of features. Depends on style.
+                Sorted list of features. Depends on k.
         """
-        self.__score_features(X, measure, style)
-        self.__rank_features(self.__scores, style)
-        return self.__scores, self.__ranked_features
+        self.__score_features(X, measure, k)
+        self.__rank_features(self.__desc, k)
+        return self.__desc, self.__ranked_features
